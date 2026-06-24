b39d64f84fc3150a8e34114da44c0f8a1b1efc8e
ezrateshale <ezra.teshale12@gmail.com>
2026-06-24 08:57:45 -0500

    feat:CheckOutRequest DTO setup

---
+++ src/main/java/org/yearup/DTO/CheckoutRequest.java
@@ -0,0 +1,25 @@
+package org.yearup.DTO;
+
+public class CheckoutRequest {
+    private String address;
+    private String city;
+    private String state;
+    private Long zipCode;
+    private double shippingAmount;
+
+    // Getters & Setters
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public Long getZipCode() { return zipCode; }
+    public void setZipCode(Long zipCode) { this.zipCode = zipCode; }
+
+    public double getShippingAmount() { return shippingAmount; }
+    public void setShippingAmount(double shippingAmount) { this.shippingAmount = shippingAmount; }
+}
